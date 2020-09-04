f82c38f7aae1a70df381444e711bcb024be92e65
Jayin <1035933250@qq.com>
2020-09-04 11:58:35 +0800

    完善初始状态及加载显示

--- components/scroll-view/index.js
+++ components/scroll-view/index.js
@@ -14,25 +14,23 @@ Component({
   properties: {
     refreshText: { type: String, value: "刷新中..." }, // 刷新时显示文本
     useRefresherSlot: {type: Boolean, value: false}, // 使用自定义的刷新动画
-    lowerLoadingText: { type: String, value: "加载中..." }, // 加载中
+    lowerLoadingText: { type: String, value: "加载中..." }, // 加载中显示文本
     useLowerLoadingSlot: { type: Boolean, value: false }, // 使用自定义底部加载
 
     // 返回顶部按钮
     useTopSlot: { type: Boolean, value: false }, // 使用自定义返回顶部
     topColor: { type: String, value: 'inherit' }, // 返回顶部颜色
-
     showTopHoverButton: { type: Boolean, value: true }, // 是否显示悬浮按钮
     minShowTopHeight: { type: Number, value: 200 }, // 最小显示返回顶部按钮的高度
   },
   data: {
+    isInit: false, // 是否首次加载完成
     refresherHeight: -1, // 刷新动画控件高度
     currentTop: 0, // 当前的top值
 
     refresherThreshold: 0.7, // 进入刷新状态阈值，top到达 阈值 * refresherHeight 时就触发刷新
-    refresherTriggered: false, // 是否进入刷新状态
+    refresherTriggered: true, // 是否进入刷新状态
     lowerLoadTriggered: false, // 是否进入底部加载状态
-
-    scrollWithAnimation: false, // 是否开启滚动动画
     
     showTop: false, // 显示返回顶部
   },
@@ -52,21 +50,20 @@ Component({
       } else if (scrollTop > toTopScrollHeight) {
         scrollIsZero = false;
         return; // 如果还未到达临界值直接退出
-      } else if (!scrollIsZero) {
+      } else if (!scrollIsZero) { // 如果存在定时器则清除
         if (this.scrollEndTimer) clearTimeout(this.scrollEndTimer);
       }
-
-      /** 如果还未到0点并且不是刷新状态时设置0点状态并返回 */
-      if (!scrollIsZero && !this.data.refresherTriggered) {
+      
+      /** 如果还未到0点并且不是刷新状态时设置0点状态并返回 或者 处于底部加载状态 */
+      if (!scrollIsZero && !this.data.refresherTriggered || this.data.lowerLoadTriggered) {
         this.scrollToTop(event);
         return;
       }
 
       let cThreshold = this.data.refresherHeight - scrollTop; // 比较顶部距离时不需要添加缓冲值
       if (this.data.refresherThreshold * this.data.refresherHeight <= cThreshold) { // 触发加载状态
-        this.startRefresh();
         // 触发刷新事件，并返回当前对象，可通过对象中的 stopRefresh 方法停止刷新
-        this.triggerEvent("refresh", {that: this});
+        this.startRefresh(true);
         return;
       }
 
@@ -89,12 +86,13 @@ Component({
       }
     },
     // 开始刷新状态
-    startRefresh: function () {
+    startRefresh: function (trigger) {
       this.setData({ refresherTriggered: true, currentTop: 0 });
+      if (trigger) this.triggerEvent("refresh", {that: this});
     },
     // 停止刷新状态
     stopRefresh: function () {
-      this.setData({ currentTop: this.data.refresherHeight, refresherTriggered: false });
+      this.setData({ currentTop: this.data.refresherHeight, refresherTriggered: false, isInit: true });
     },
     // 滚动至顶部
     scrollToTop: function (event) {
@@ -116,10 +114,13 @@ Component({
   },
   lifetimes: {
     attached: function () {
+      // 重置全局变量
+      scrollIsZero = false, toZeroTimestamp = 0;
       // 获取刷新元素高度
       this.createSelectorQuery().select(".__refresh-anim-container").boundingClientRect(res => {
-        this.setData({ refresherHeight: res.height, currentTop: res.height }, () => {
-          this.setData({scrollWithAnimation: true});
+        this.setData({ refresherHeight: res.height }, function () {
+          // 默认进来就会触发加载时间
+          this.startRefresh(true);
         });
       }).exec();
     }
